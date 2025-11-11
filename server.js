d9a8e08918b9f53f8f49aef9fd83515251be2ce0
lore <lorenzo.sevellina@neu.edu.ph>
2025-11-11 15:40:56 +0800

    Removed line app listen for a fix

--- server.js
+++ server.js
@@ -92,7 +92,6 @@ async function startServer() {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('✅ Connected to MongoDB Atlas');
-    app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
   } catch (err) {
     console.error('❌ Failed to connect:', err.message);
   }
